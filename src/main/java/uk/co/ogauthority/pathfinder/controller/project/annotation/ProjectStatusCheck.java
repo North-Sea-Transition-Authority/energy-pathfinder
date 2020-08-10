32ae3fede1df81c4e82b53b135f7f611b9008819
lmckeown <lewis.mckeown@fivium.co.uk>
2020-08-10 14:17:44 +0100

    Fix checkstyle rule violation

--- src/main/java/uk/co/ogauthority/pathfinder/controller/project/annotation/ProjectStatusCheck.java
+++ src/main/java/uk/co/ogauthority/pathfinder/controller/project/annotation/ProjectStatusCheck.java
@@ -7,7 +7,8 @@ import java.lang.annotation.Target;
 import uk.co.ogauthority.pathfinder.model.enums.project.ProjectStatus;
 
 /**
- * Annotation to be used in conjunction with {@link uk.co.ogauthority.pathfinder.mvc.argumentresolver.ProjectContextArgumentResolver} on controller
+ * Annotation to be used in conjunction with
+ * {@link uk.co.ogauthority.pathfinder.mvc.argumentresolver.ProjectContextArgumentResolver} on controller
  * methods to restrict processing of the method to applications at a specific status.
  */
 @Retention(RetentionPolicy.RUNTIME)
