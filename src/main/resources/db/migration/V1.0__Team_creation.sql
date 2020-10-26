9aa329387eb85c6a2164f0d2705242ae6e300342
Daniel Ashworth <daniel.ashworth@fivium.co.uk>
2020-10-26 10:19:39 +0000

    Merged in feature/PAT-167/only-create-organisation-teams-for-existing-pathfinder-organisations (pull request #78)
    feature/PAT-167/only-create-organisation-teams-for-existing-pathfinder-organisations
    
    * - Update the team creation script to only created pathfinder organisation teams where an organisation had a legacy pathfinder team
    
    Approved-by: Danny Betts
    Approved-by: Lewis McKeown

--- src/main/resources/db/migration/V1.0__Team_creation.sql
+++ src/main/resources/db/migration/V1.0__Team_creation.sql
@@ -1,5 +1,11 @@
 DECLARE
 
+  K_NEW_REGULATOR_TEAM_TYPE CONSTANT VARCHAR2(4000) := 'PATHFINDER_REGULATOR_TEAM';
+  K_NEW_REGULATOR_TEAM_NAME CONSTANT VARCHAR2(4000) := 'Pathfinder regulator team';
+  K_OLD_ORGANISATION_TEAM_TYPE CONSTANT VARCHAR2(4000) := 'PATH_OPERATOR_TEAM';
+  K_NEW_ORGANISATION_TEAM_TYPE CONSTANT VARCHAR2(4000) := 'PATHFINDER_ORGANISATION_TEAM';
+  K_NEW_ORGANISATION_TEAM_NAME CONSTANT VARCHAR2(4000) := 'Pathfinder organisation team';
+
   l_organisation_resource_id NUMBER;
   l_regulator_resource_id NUMBER;
 
@@ -10,7 +16,7 @@ BEGIN
     SELECT r.id
     INTO l_regulator_resource_id
     FROM decmgr.resources r
-    WHERE r.res_type = 'PATHFINDER_REGULATOR_TEAM';
+    WHERE r.res_type = K_NEW_REGULATOR_TEAM_TYPE;
 
   EXCEPTION WHEN NO_DATA_FOUND THEN
 
@@ -21,9 +27,9 @@ BEGIN
   IF l_regulator_resource_id IS NULL THEN
 
     l_regulator_resource_id := decmgr.contact.create_default_team(
-      p_resource_type => 'PATHFINDER_REGULATOR_TEAM'
-    , p_resource_name => 'Pathfinder regulator team'
-    , p_resource_desc => 'Pathfinder regulator team'
+      p_resource_type => K_NEW_REGULATOR_TEAM_TYPE
+    , p_resource_name => K_NEW_REGULATOR_TEAM_NAME
+    , p_resource_desc => K_NEW_REGULATOR_TEAM_NAME
     , p_creating_wua_id => 1
     );
 
@@ -45,16 +51,24 @@ BEGIN
       SELECT 1
       FROM decmgr.resource_usages_current ruc
       JOIN decmgr.xview_resources xr ON xr.res_id = ruc.res_id
-      WHERE xr.res_type = 'PATHFINDER_ORGANISATION_TEAM'
+      WHERE xr.res_type = K_NEW_ORGANISATION_TEAM_TYPE
+      AND ruc.uref = cog.id || '++REGORGGRP'
+    )
+    -- Only create if the organisation had a legacy pathfinder team
+    AND EXISTS(
+      SELECT 1
+      FROM decmgr.resource_usages_current ruc
+      JOIN decmgr.xview_resources xr ON xr.res_id = ruc.res_id
+      WHERE xr.res_type = K_OLD_ORGANISATION_TEAM_TYPE
       AND ruc.uref = cog.id || '++REGORGGRP'
     )
   )
   LOOP
 
     l_organisation_resource_id := decmgr.contact.create_default_team(
-      p_resource_type => 'PATHFINDER_ORGANISATION_TEAM'
-    , p_resource_name => 'Pathfinder organisation team'
-    , p_resource_desc => 'Pathfinder organisation team - ' || org_group.org_name
+      p_resource_type => K_NEW_ORGANISATION_TEAM_TYPE
+    , p_resource_name => K_NEW_ORGANISATION_TEAM_NAME
+    , p_resource_desc => K_NEW_ORGANISATION_TEAM_NAME || ' - ' || org_group.org_name
     , p_uref => org_group.uref
     , p_uref_purpose => 'PRIMARY_DATA'
     , p_creating_wua_id => 1
