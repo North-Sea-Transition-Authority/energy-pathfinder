78bd798d0e555c622e371b8a43bfd8b837449804
lmckeown <lewis.mckeown@fivium.co.uk>
2020-11-20 15:45:30 +0000

    Use static Yes No references in enum Remove unnecessary line breaks

--- src/main/java/uk/co/ogauthority/pathfinder/model/enums/project/tasks/tasklistquestions/TaskListSectionAnswer.java
+++ src/main/java/uk/co/ogauthority/pathfinder/model/enums/project/tasks/tasklistquestions/TaskListSectionAnswer.java
@@ -1,35 +1,35 @@
 package uk.co.ogauthority.pathfinder.model.enums.project.tasks.tasklistquestions;
 
+import uk.co.ogauthority.pathfinder.util.StringDisplayUtil;
+
 /**
  * Enum to store the two possible answers for any {@link TaskListSectionQuestion}. A list of these
  * is stored against the {@link uk.co.ogauthority.pathfinder.model.entity.project.tasks.ProjectTaskListSetup}
  * to keep track of what a user has asked to be included on their project.
  */
 public enum TaskListSectionAnswer {
-  UPCOMING_TENDERS_YES("Yes"),
-  UPCOMING_TENDERS_NO("No"),
-  AWARDED_CONTRACTS_YES("Yes"),
-  AWARDED_CONTRACTS_NO("No"),
-  COLLABORATION_OPPORTUNITIES_YES("Yes"),
-  COLLABORATION_OPPORTUNITIES_NO("No"),
-  WELLS_YES("Yes"),
-  WELLS_NO("No"),
-  PLATFORM_FPSO_YES("Yes"),
-  PLATFORM_FPSO_NO("No"),
-  INTEGRATED_RIGS_YES("Yes"),
-  INTEGRATED_RIGS_NO("No"),
-  SUBSEA_INFRASTRUCTURE_YES("Yes"),
-  SUBSEA_INFRASTRUCTURE_NO("No"),
-  PIPELINES_YES("Yes"),
-  PIPELINES_NO("No");
+  UPCOMING_TENDERS_YES(StringDisplayUtil.YES),
+  UPCOMING_TENDERS_NO(StringDisplayUtil.NO),
+  AWARDED_CONTRACTS_YES(StringDisplayUtil.YES),
+  AWARDED_CONTRACTS_NO(StringDisplayUtil.NO),
+  COLLABORATION_OPPORTUNITIES_YES(StringDisplayUtil.YES),
+  COLLABORATION_OPPORTUNITIES_NO(StringDisplayUtil.NO),
+  WELLS_YES(StringDisplayUtil.YES),
+  WELLS_NO(StringDisplayUtil.NO),
+  PLATFORM_FPSO_YES(StringDisplayUtil.YES),
+  PLATFORM_FPSO_NO(StringDisplayUtil.NO),
+  INTEGRATED_RIGS_YES(StringDisplayUtil.YES),
+  INTEGRATED_RIGS_NO(StringDisplayUtil.NO),
+  SUBSEA_INFRASTRUCTURE_YES(StringDisplayUtil.YES),
+  SUBSEA_INFRASTRUCTURE_NO(StringDisplayUtil.NO),
+  PIPELINES_YES(StringDisplayUtil.YES),
+  PIPELINES_NO(StringDisplayUtil.NO);
 
   private final String answerValue;
 
   TaskListSectionAnswer(String answerValue) {
     this.answerValue = answerValue;
   }
-
-
 
   public String getAnswerValue() {
     return answerValue;
