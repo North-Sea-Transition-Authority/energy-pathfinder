6650f4d823f30bf075dc2680cb012fc2596e4ac7
lmckeown <lewis.mckeown@fivium.co.uk>
2020-11-24 11:46:43 +0000

    Remove unused import

--- src/main/java/uk/co/ogauthority/pathfinder/service/dashboard/DashboardService.java
+++ src/main/java/uk/co/ogauthority/pathfinder/service/dashboard/DashboardService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.co.ogauthority.pathfinder.auth.AuthenticatedUserAccount;
 import uk.co.ogauthority.pathfinder.model.entity.dashboard.DashboardProjectItem;
-import uk.co.ogauthority.pathfinder.model.enums.project.ProjectStatus;
 import uk.co.ogauthority.pathfinder.model.view.dashboard.DashboardProjectItemView;
 import uk.co.ogauthority.pathfinder.service.team.TeamService;
 
