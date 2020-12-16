3c5e9a6c9521b76f1de0f00531632a4ac0e7ed38
Harry Devane <harry.devane@fivium.co.uk>
2020-12-16 13:36:43 +0000

    Remove unused imports in TaskListService

--- src/main/java/uk/co/ogauthority/pathfinder/service/project/tasks/TaskListService.java
+++ src/main/java/uk/co/ogauthority/pathfinder/service/project/tasks/TaskListService.java
@@ -2,23 +2,11 @@ package uk.co.ogauthority.pathfinder.service.project.tasks;
 
 import static org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder.on;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import org.apache.commons.collections4.SetUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 import uk.co.ogauthority.pathfinder.controller.project.CancelDraftController;
 import uk.co.ogauthority.pathfinder.model.entity.project.ProjectDetail;
-import uk.co.ogauthority.pathfinder.model.enums.project.tasks.ProjectTask;
-import uk.co.ogauthority.pathfinder.model.enums.project.tasks.ProjectTaskGroup;
-import uk.co.ogauthority.pathfinder.model.view.tasks.TaskListEntry;
-import uk.co.ogauthority.pathfinder.model.view.tasks.TaskListGroup;
 import uk.co.ogauthority.pathfinder.mvc.ReverseRouter;
 
 @Service
