de83dd741f0f0fd28d199a2d9e4fa5f6075df9ca
Harry Devane <harry.devane@fivium.co.uk>
2021-01-06 16:59:11 +0000

    Add private constructor to LicenceBlockUtil

--- src/main/java/uk/co/ogauthority/pathfinder/util/LicenceBlockUtil.java
+++ src/main/java/uk/co/ogauthority/pathfinder/util/LicenceBlockUtil.java
@@ -4,6 +4,10 @@ import org.apache.commons.lang3.StringUtils;
 
 public class LicenceBlockUtil {
 
+  private LicenceBlockUtil() {
+    throw new IllegalStateException("LicenceBlockUtil is a utility class and should not be instantiated");
+  }
+
   public static String getSortKey(String quadrantNumber, String blockNumber, String blockSuffix) {
     return StringUtils.leftPad(quadrantNumber, 10, "0") +
         StringUtils.leftPad(blockNumber, 10, "0") +
