50fcac26d247ef35299bd7f66af678a05d3e42eb
lmercer <lucy.mercer@fivium.co.uk>
2021-04-22 10:37:53 +0100

    - Updated flyway and indentation issues from PR

--- src/main/resources/db/migration/V8.5__add_contact_band_column_to_forward_work_plan_table.sql
+++ src/main/resources/db/migration/V8.5__add_contact_band_column_to_forward_work_plan_table.sql
@@ -1,8 +1,2 @@
 ALTER TABLE ${datasource.user}.work_plan_upcoming_tenders
 ADD(contract_band VARCHAR2(4000));
-
-UPDATE ${datasource.user}.work_plan_upcoming_tenders wput
-SET wput.contract_band = 'LESS_THAN_5M';
-
-ALTER TABLE ${datasource.user}.work_plan_upcoming_tenders
-MODIFY(contract_band VARCHAR2(4000) NOT NULL);
