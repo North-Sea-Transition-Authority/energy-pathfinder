1227fae3c69416a9a10165112becf1cc9baf1fdb
Daniel Ashworth <daniel.ashworth@fivium.co.uk>
2022-04-22 07:56:22 +0000

    Merged in master (pull request #553)
    Added ResourceUrlEncodingFilter to WebMvcConfig so assets hashes are added correctly
    
    * Added ResourceUrlEncodingFilter to WebMvcConfig so assets hashes are added correctly
    
    Approved-by: Danny Betts
    Approved-by: James Barnett

--- src/main/java/uk/co/ogauthority/pathfinder/config/WebMvcConfig.java
+++ src/main/java/uk/co/ogauthority/pathfinder/config/WebMvcConfig.java
@@ -3,12 +3,14 @@ package uk.co.ogauthority.pathfinder.config;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 import uk.co.ogauthority.pathfinder.analytics.AnalyticsService;
 import uk.co.ogauthority.pathfinder.mvc.ProjectRouteInterceptor;
@@ -82,4 +84,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   }
 
+  @Bean
+  public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+    return new ResourceUrlEncodingFilter();
+  }
 }
